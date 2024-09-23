cf2648968f6c15d946fcf9a8610d752902883d52
Jaro111 <jarokark@protonmail.com>
2024-09-23 19:03:09 +0100

    chore: folders created and modules installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Foter } from "./components/Footer/Footer";
 
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <p>APP</p>
-    </>
-  );
+  return <BrowserRouter basename=""></BrowserRouter>;
 }
 
 export default App;
